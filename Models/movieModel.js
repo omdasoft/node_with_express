11e1e55b9abba56f7f36c988019c1e4a03d0e526
Emad Aldin <eabdulwahid@wallpostdev.com>
2024-09-11 14:49:30 +0400

    add mongo post and pre hooks

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 const moviesSchema = new mongoose.Schema({
     name: {
@@ -53,8 +54,36 @@ const moviesSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: [true, 'Price field is required']
-    }
-})
+    },
+    createdBy: String
+}, {
+    toJSON: {virtuals: true}, //enable the virtuals propeities to return 
+    toObject: {virtuals: true} //enable virtuals to be avilable in the view
+});
+
+moviesSchema.virtual('durationInHours').get(function() { //virtuals can not be used in the query the data
+    return this.duration / 60;
+});
+
+//Middleware that run before save event is fired, before calling save and create  (pre hook)
+moviesSchema.pre('save', function(next) {
+    this.createdBy = 'Emad';
+    next();
+});
+
+//we can add more hooks for the same event 
+// moviesSchema.pre('save', function(next) {
+//    //ADD New Logic
+// });
+
+//Middleware that run aftert the query is saved or created (post hook)
+moviesSchema.post('save', function (doc, next) {
+    const data = `A new movie with name ${doc.name} has been created by user ${doc.createdBy}\n`;
+    fs.writeFileSync('./Log/log.txt', data, {flag: 'a'}, (err) => {
+        console.log(err.message);
+    });
+    next();
+});
 
 const Movie = mongoose.model('Movie', moviesSchema);
 
